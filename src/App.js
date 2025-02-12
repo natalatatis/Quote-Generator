a18a1ebea9c2e30d039050da7271af830b7151e6
Natalia Sosa <natalia.sosa@galileo.edu>
2025-02-12 10:24:08 -0600

    Added a new quote button and more quotes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Card, Typography } from "@mui/material";
+import { Container, Card, Typography, Button } from "@mui/material";
 //font awesome for star wars and harry potter icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faJedi, faMagic } from "@fortawesome/free-solid-svg-icons";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
 
 //Some quotes from movies
 const quotes = [
   "'It's LeviOooosa, not LeviosA!' – Hermione Granger",
   "'Hello There!' – Obi Wan Kenobi",
-  "'Yer a wizard Harry!' – Hagrid",
+  "'Yer a wizard 'Arry!' – Hagrid",
   "'pip pup pip pup!' – R2-D2",
   "'I am your father!' – Darth Vader",
-  "'Execute order 66!' – Palpatine",
+  "'Execute Order 66!' – Palpatine",
   "'Help me, Obi Wan Kenobi, you're my only hope.' – Leia Organa",
-  "'Do. Or do not. There is no try' – Master Yoda",
+  "'Do. Or do not. There is no try.' – Master Yoda",
   "'Dobby is free!' – Dobby",
   "'Just because you have the emotional range of a teaspoon doesn’t mean we all have'. – Hermione Granger",
+  "'This is the way.' - The Mandalorian",
+  "'I've got a bad feeling about this.' - Most Star Wars characters",
+  "'It's a trap!' - Admirant Ackbar",
+  "'I solemnly swear that I am up to no good.' - Harry Potter",
+  "'You're just as sane as I am' - Luna Lovegood",
+  "'He's not Fred, I am' - George Weasley",
+  "'I find your lack of faith disturbing.' - Darth Vader",
+  "'I've always wanted to use that spell!' - Minerva McGonagall",
 ];
 
 //This function will return a random quote from the quotes array
@@ -27,6 +36,7 @@ const getRandomQuote = () => {
 export default function App() {
   const [quote, setQuotes] = useState(getRandomQuote());
 
+  //Sets a new quote every 5 seconds
   useEffect(() => {
     const interval = setInterval(() => {
       setQuotes(getRandomQuote());
@@ -38,21 +48,33 @@ export default function App() {
   return (
     <>
       {/* Icons related to the quotes*/}
-      <div style={{ fontSize: "50px", textAlign: "center" }}>
+      <div style={{ fontSize: "50px", textAlign: "center", marginTop: "50px" }}>
         <FontAwesomeIcon icon={faJedi} title="Jedi Order" />
         <FontAwesomeIcon icon={faMagic} title="Magic Wand" />
       </div>
 
-      <Container sx={{ textAlign: "center", mt: 5 }}>
-        <Typography variant="h4" color="#e67151">
+      {/*Title*/}
+      <Container sx={{ textAlign: "center", mt: 15 }}>
+        <Typography variant="h4" color="#1029af" sx={{ fontWeight: "bold" }}>
           Very profound quote generator
         </Typography>
-        <Card sx={{ bgcolor: "#ebf6c2", p: 3, boxShadow: 5, mt: 5 }}>
-          <Typography variant="h5" color="#2f6871">
+        {/*Quotes*/}
+        <Card sx={{ bgcolor: "#5c77ef", p: 3, boxShadow: 5, mt: 5 }}>
+          <Typography variant="h5" color="white">
             {quote}
           </Typography>
         </Card>
+        {/*New Quote Button*/}
+        <Button
+          variant="contained"
+          sx={{ mt: 2, bgcolor: "#eb4d62", color: "white" }}
+          endIcon={<AutorenewIcon />}
+          size="large"
+          onClick={() => setQuotes(getRandomQuote())}
+        >
+          New Quote
+        </Button>
       </Container>
     </>
   );
-}
+} //#efd45c
